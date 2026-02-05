3ac472e183a6c3bec277263e805d1e0991326500
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2026-02-05 14:30:39 +0530

    added input focus

--- Task Manager/src/components/TaskDash.jsx
+++ Task Manager/src/components/TaskDash.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import DisplayTasks from './DisplayTasks'
 import { useTasks } from '../context/TaskContext'
 import Navbar from './Navbar';
+import { useRef } from 'react';
 
 function TaskDash() {
 
@@ -14,13 +15,23 @@ function TaskDash() {
         setText(''); 
     };
 
+    const inputEl = useRef(null);
+
+    function focus(){
+        inputEl.current.focus();
+    }
+
+    const handler = ()=>{
+        
+    }
+
     
     
     return (
         <div>
             <Navbar/>
-            <input type="text" value = {text} onChange={(e) => setText(e.target.value)}/>
-            <button onClick={handleAdd}>Add Task</button>
+            <input type="text" ref={inputEl} value = {text} onChange={(e) => setText(e.target.value)}/>
+            <button onClick={() => { handleAdd(); focus(); }}>Add Task</button>
             <DisplayTasks />
         </div>
     )
